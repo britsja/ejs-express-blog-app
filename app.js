b3f32a39d9ea7f92e4b2cdffc5a966aa75e34252
Jan <pcerservices@gmail.com>
2023-07-12 14:42:37 +0200

    Show post excerp on homepage for each post

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ app.listen(3000, function() {
     console.log("Listening on port 3000");
 })
 
-const blogPostCollection = [['Example title 1', 'Example post data 1'],['Example Blog title 2', 'Post 2 data']];
+const blogPostCollection = [
+    ['Lorem Ipsum Post', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque a temporibus laboriosam, assumenda facere perferendis quos doloribus, sequi, repellat eveniet blanditiis nostrum pariatur ut ipsam nam aliquid vel modi. Optio!'],
+    ['Perspiciatis', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?']
+];
 
 app.get("/", function(req, res) {
     const title = "Blog Home Page";
