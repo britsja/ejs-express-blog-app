1ac3bed291209129c57e6896ace40b9b397f774b
Jan <pcerservices@gmail.com>
2023-07-12 14:26:04 +0200

    Set up blog post file and blog creating functionality

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ app.listen(3000, function() {
     console.log("Listening on port 3000");
 })
 
+const blogPostCollection = [['Example title 1', 'Example post data 1'],['Example Blog title 2', 'Post 2 data']];
+
 app.get("/", function(req, res) {
     const title = "Blog Home Page";
-    res.render("index", {title: title});
+    console.log(blogPostCollection);
+    res.render("index", {title: title, blogPostCollection: blogPostCollection});
+    
 })
 
 app.get("/about", function(req, res) {
@@ -31,4 +35,20 @@ app.get("/contact", function(req, res) {
 app.get("/create", function(req, res) {
     const title = "Create a Blog Post";
     res.render("create", {title: title});
+})
+
+app.get("/blogpost", function(req, res) {
+    const title = req.query.title;
+    const blogPostNumber = req.query.blogNumber;
+    const blogData = blogPostCollection[blogPostNumber][1];
+    res.render("blogpost", {title: title, blogData: blogData});
+})
+
+app.post("/create", function(req, res) {
+    const blogPost = req.body.newPost;
+    const postTitle = req.body.postTitle;
+    const postData = [postTitle, blogPost];
+    blogPostCollection.push(postData);  
+    res.redirect("/");
+
 })
